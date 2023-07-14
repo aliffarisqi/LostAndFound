86c52d3471ed1b5d1754a5fc24d7863f6987dca1
Bayu Alif Farisqi <bayualiev@gmail.com>
2023-07-14 10:47:03 +0700

    Initial Commit

---
+++ LostAndfound/LostAndfoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  LostAndfoundApp.swift
+//  LostAndfound
+//
+//  Created by Bayu Alif Farisqi on 14/07/23.
+//
+
+import SwiftUI
+
+@main
+struct LostAndfoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
